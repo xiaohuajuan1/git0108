76c930a429597f149ef147476bd01f0e740a4c0b
atguigu <atguigu@atguigu.com>
2021-05-25 21:27:18 +0800

    2222

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("第一次使用idea操作git");
         System.out.println("使用hot-fix进行修改");
+        System.out.println("使用hot-fix进行合并");
 
     }
 }
