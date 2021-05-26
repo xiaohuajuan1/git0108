6b709c5f610607934052f35383b9af96115a9431
xiaohuajuan1 <84754272+xiaohuajuan1@users.noreply.github.com>
2021-05-26 09:00:07 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,6 +3,7 @@ public class test {
         System.out.println("第一次使用idea操作git");
         System.out.println("使用hot-fix进行修改");
         System.out.println("使用master进行合并");
+        System.out.println("在远程库修改 然后再idea进行pull拉下来");
 
     }
 }
